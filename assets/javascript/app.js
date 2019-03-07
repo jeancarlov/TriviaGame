79c64b4fb651a3de8eb4975e6a7fa7495fa11bab
Jean Valderrama <jeanvalderrama@Jeans-MacBook-Pro.local>
2019-03-07 15:39:11 -0500

    working in phase 3

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,46 +8,55 @@ $(document).ready(function () {
     var correctAnswers = 0;
     var incorrectAnswers = 0;
     var unAnswered = 0;
-    var count = 45 
+    var count = 45
+
+
+    
+
 
 
     // game questions and answers. Check if is better to have the questions before the start game fn or inside the wrapper.
+
     var myQuestions = [
         {
-            question: 'where is mount everest located at?',
-            answers: {    // change this to an array so that i could use the index
+            question: "where is mount everest located at?",
+            answers: {
                 a: 'canada',
                 b: 'nepal',
-                c: 'china',
+                c: 'china'
             },
-            correctAnswer: this.answers[1], // This way may be better because it prevents errors
+            correctAnswer: 'b'
+        },
+        {
+            question: "moscow is the capital of what country?",
+            answers: {
+                a: 'russia',
+                b: 'venezuela',
+                c: 'germany',
+            },
+            correctAnswer: 'a'
+        },
+        {
+            question: "berlin is the capital of what country?",
+            answers: {
+                a: 'germany',
+                b: 'france',
+                c: 'italy'
+            },
+            correctAnswer: 'a'
+        },
+        {
+            question: "rome is the capital of what country?",
+            answers: {
+                a: 'germany',
+                b: 'france',
+                c: 'italy'
+            },
+            correctAnswer: 'c'
         },
 
-        {
-            question: 'moscow is the capital of what country?',
-            answers: ['russia', 'venezuela', 'germany'],
-            correctAnswer: this.answers[0],
-        },
-
-        {
-            question: 'berlin is the capital of what country?',
-            answers: ['germany', 'france', 'italy'],
-            correctAnswer: this.answers[0],
-        },
-
-        {
-            question: 'madrid is the capital of what country?',
-            answers: ['portugal', 'spain', 'england'],
-            correctAnswer: this.answers[1],
-        },
-
-        {
-            question: 'rome is the capital of what country?',
-            answers: ['russia', 'japan', 'italy'],
-            correctAnswer: this.answers[2],
-        },
     ];
-
+    console.log(myQuestions)
     // I need to put a reset funtion for the game to star again after the results have been show 
 
     // start funtion when the button is click phase 1
@@ -58,49 +67,58 @@ $(document).ready(function () {
 
     });
     // timer funtion for the seconds to decrease the variable count by a 1000
-    function timer (){
+    function timer() {
         count--;
-        $('#timer').html('Time remaining: ' + '00' + count + "seconds");
-        if( count <=0){
-        clearInterval(counter)
-        //check if there is a piece missing for this function to work
+        $('#timer').html('Time remaining: ' + count + " seconds");
+        if (count <= 0) {
+            clearInterval(counter)
+            //check if there is a piece missing for this function to work
         }
     }
 
     // phase 2  showing the questions and answer with the time counter in seconds running
     function showQuestions() {
-        $('#questions-container').html(myQuestions[0].question);
-        question++;
-        myAnswers();
+        // for (var i =0; i< myQuestions.length; i++){
+        //     $('#questions-container').append(myQuestions[i].question +"<br>"); 
+        //     // $('questions-container').append()
+        // }
+        $('#questions-container').append(myQuestions[0].question + '<br>');
+        $('#questions-container').append('<button>' + myQuestions[0].answers.a + '</button>');
+        $('#questions-container').append('<button>' + myQuestions[0].answers.b + '</button>');
+        $('#questions-container').append('<button>' + myQuestions[0].answers.c + '</button><br>')
+        $('#questions-container').append(myQuestions[1].question + '<br>');
+        $('#questions-container').append('<button>' + myQuestions[1].answers.a + '</button>');
+        $('#questions-container').append('<button>' + myQuestions[1].answers.b + '</button>');
+        $('#questions-container').append('<button>' + myQuestions[1].answers.c + '</button><br>')
+        $('#questions-container').append(myQuestions[2].question + '<br>');
+        $('#questions-container').append('<button>' + myQuestions[2].answers.a + '</button>');
+        $('#questions-container').append('<button>' + myQuestions[2].answers.b + '</button>');
+        $('#questions-container').append('<button>' + myQuestions[2].answers.c + '</button><br>')
+        $('#questions-container').append(myQuestions[3].question + '<br>');
+        $('#questions-container').append('<button>' + myQuestions[3].answers.a + '</button>');
+        $('#questions-container').append('<button>' + myQuestions[3].answers.b + '</button>');
+        $('#questions-container').append('<button>' + myQuestions[3].answers.c + '</button><br>')
+        // question++;
 
-    }
-
-    // This funtion is to create new div button for the html to display the answers 
-    function myAnswers() {
-        var answersArr = myQuestions[0].answers;
-
-        for (var i = 0; i < answersArr.length; i++) {
-            var button = $('<button>');
-            button.text(answersArr[i]);
-            button.attr('data-value', i);
-            $('#answer-container');
-        }
-        console.log(myAnswers);
-    }
-
-    $('#answer-container').on('click', 'button', function(event){
-        userPick = $('#answer-container').data('value');
-        myQuestions[0].correctAnswer;
-        if( userPick === myQuestions[0].correctAnswer){
+        if (myQuestions[0].question === myQuestions.correctAnswer[0]){
             correctAnswers++;
-        } else if ( userPick !== myQuestions[0].correctAnswer){
-            incorrectAnswers
-        }
-    })
+            // $('.results').text(myQuestions.correctAnswers[0])
+        } else if (myQuestions[0].question !== myQuestions.correctAnswer[0]) {
+            incorrectAnswers++;
+        };
+
+
+
+    }
+
+
+
+
+
 
 
     // notes: phase three show the message all done, show results, and reset funtion for the game to start again
 
 
-},
+})
 
